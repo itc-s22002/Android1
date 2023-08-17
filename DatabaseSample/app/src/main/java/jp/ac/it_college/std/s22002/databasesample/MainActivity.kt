b6252d2cfd79c323f9fa3ff9112f6b2d964171af
s22002 <s22002@std.it-college.ac.jp>
2023-08-17 16:26:22 +0900

    10.1まで

--- DatabaseSample/app/src/main/java/jp/ac/it_college/std/s22002/databasesample/MainActivity.kt
+++ DatabaseSample/app/src/main/java/jp/ac/it_college/std/s22002/databasesample/MainActivity.kt
@@ -2,10 +2,31 @@ package jp.ac.it_college.std.s22002.databasesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import jp.ac.it_college.std.s22002.databasesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initList(binding.lvCooktail)
+    }
+
+    private fun initList(list: RecyclerView){
+        val data = resources.getStringArray(R.array.tv_cooktail_list)
+        val adapter = CocktailAdapter(data.toList()){ pos, name ->
+            binding.tvCooktailName.text = name
+        }
+        val manager = LinearLayoutManager(this)
+        list.adapter = adapter
+        list.layoutManager = manager
+        list.addItemDecoration(
+            DividerItemDecoration(this, manager.orientation)
+        )
     }
 }
