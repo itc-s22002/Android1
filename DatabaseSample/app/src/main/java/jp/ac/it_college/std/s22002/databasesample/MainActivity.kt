9ad949eb85b1f7c732e0bfe5e87ea4276c3bbe32
s22002 <s22002@std.it-college.ac.jp>
2023-08-22 16:45:39 +0900

    10章終了

--- DatabaseSample/app/src/main/java/jp/ac/it_college/std/s22002/databasesample/MainActivity.kt
+++ DatabaseSample/app/src/main/java/jp/ac/it_college/std/s22002/databasesample/MainActivity.kt
@@ -2,6 +2,7 @@ package jp.ac.it_college.std.s22002.databasesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,18 +10,34 @@ import jp.ac.it_college.std.s22002.databasesample.databinding.ActivityMainBindin
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var _helper: DatabaseHelper
+
+    private var _cocktailId :Long  = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        _helper = DatabaseHelper(this)
+
         initList(binding.lvCooktail)
+
+        binding.btnSave.setOnClickListener(::onSaveButton)
+    }
+
+    override fun onDestroy() {
+        _helper.close()
+        super.onDestroy()
     }
 
     private fun initList(list: RecyclerView){
         val data = resources.getStringArray(R.array.tv_cooktail_list)
         val adapter = CocktailAdapter(data.toList()){ pos, name ->
             binding.tvCooktailName.text = name
+
+            _cocktailId = pos.toLong()
+
+            binding.etNote.setText(loadCocktailMemos())
         }
         val manager = LinearLayoutManager(this)
         list.adapter = adapter
@@ -28,5 +45,54 @@ class MainActivity : AppCompatActivity() {
         list.addItemDecoration(
             DividerItemDecoration(this, manager.orientation)
         )
+    }
+
+    private fun onSaveButton(view: View){
+        val note = binding.etNote.text.toString()
+        val db = _helper.writableDatabase
+        val deleteSQL = """
+            | DELETE FROM cocktail_memos
+            | WHERE _id = ?
+        """.trimMargin()
+
+        db.compileStatement(deleteSQL).let {stmt ->
+            stmt.bindLong(1, _cocktailId)
+            stmt.executeUpdateDelete()
+        }
+
+        val insertSQL = """
+            | INSERT INTO cocktail_memos (_id, name, note)
+            | VALUES (?, ?, ?)
+        """.trimMargin()
+
+        db.compileStatement(insertSQL).let { stmt ->
+            stmt.bindLong(1, _cocktailId)
+            stmt.bindString(2,binding.tvCooktailName.text.toString())
+            stmt.bindString(3, note)
+            stmt.executeInsert()
+        }
+
+        binding.etNote.setText("")
+        binding.tvCooktailName.text = ""
+        _cocktailId = 0
+    }
+
+    private fun loadCocktailMemos(): String{
+        val db = _helper.readableDatabase
+
+
+        val select = """
+            | SELECT * FROM cocktail_memos
+            | WHERE _id = ?
+            """.trimMargin()
+        val cursor = db.rawQuery(select, arrayOf("$_cocktailId"))
+
+        return cursor.use {
+            if (cursor.moveToNext()){
+                cursor.getString(cursor.getColumnIndexOrThrow("note"))
+            }else{
+                ""
+            }
+        }
     }
 }
